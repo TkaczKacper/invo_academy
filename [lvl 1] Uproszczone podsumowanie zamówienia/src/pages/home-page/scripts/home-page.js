46870c842d3160617fec74a7f9a9006c7b76aca2
TkaczKacper <kacpertk2016@gmail.com>
2023-03-09 17:08:28 +0100

    1st project finish

--- [lvl 1] Uproszczone podsumowanie zamówienia/src/pages/home-page/scripts/home-page.js
+++ [lvl 1] Uproszczone podsumowanie zamówienia/src/pages/home-page/scripts/home-page.js
@@ -1,8 +1,14 @@
 /// Homepage JavaScript File
 /// Here we import all the JavaScript files we need for our homepage.
 
-import '../styles/home-page.scss'
+import "../styles/home-page.scss";
 
-import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
-
-setupCounter(document.querySelector('#counter'))
+document.addEventListener("DOMContentLoaded", function () {
+   const element = document.getElementById("payment-button");
+   element.addEventListener("click", () => {
+      element.className = "order-summary-container__payment-processing";
+      let div1 = document.createElement("div");
+      element.innerHTML = "";
+      element.appendChild(div1);
+   });
+});
