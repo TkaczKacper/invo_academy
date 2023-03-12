142ed02f15219a8b1a8f7f8374dc605386dbeb26
TkaczKacper <kacpertk2016@gmail.com>
2023-03-12 14:06:28 +0100

    cleaning up

--- [lvl 4] Spotify - rejestracja i logowanie (react)/src/App.tsx
+++ [lvl 4] Spotify - rejestracja i logowanie (react)/src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React from "react";
 import { HomePage } from "@modules";
-import { MainMenuGlobalComponent } from "@/global-components";
 
 const App: React.FC = () => (
-  <>
-    <MainMenuGlobalComponent />
-    <HomePage msg="INVO Academy Starter with React + TypeScript" />
-  </>
+   <>
+      <HomePage />
+   </>
 );
 
 export default App;
--- [lvl 4] Spotify - rejestracja i logowanie (react)/src/modules/HomeModule/pages/index.ts
+++ [lvl 4] Spotify - rejestracja i logowanie (react)/src/modules/HomeModule/pages/index.ts
@@ -1,1 +1,3 @@
-export * from './HomePage/HomePage';
+export * from "./HomePage/HomePage";
+export * from "./LoginPage/LoginPage";
+export * from "./RegisterPage/RegisterPage";
